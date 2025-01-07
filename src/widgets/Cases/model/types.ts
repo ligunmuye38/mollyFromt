c5329254d62dcae48c44c0a89233ff5f4f2c91b0
ligunmuye38 <ligun0805@hotmail.com>
2025-01-08 04:34:03 +0900

    add ICaseItemType, update ImageBgType(yello : yellow)

--- src/widgets/Cases/model/types.ts
+++ src/widgets/Cases/model/types.ts
@@ -11,7 +11,7 @@ export interface ISelectImage {
 }
 
 //Image background Type(To create case)
-export type ImageBgType = 'yello' | 'pink' | 'purple' | 'blue' | 'limit_blue'
+export type ImageBgType = 'yellow' | 'pink' | 'purple' | 'blue' | 'limit_blue'
 
 // Chance interface(To create case)
 export interface IChance {
@@ -29,4 +29,16 @@ export interface IChance {
 export interface ICaseType {
 	id: number
 	type: string
+}
+
+// Case type interface (To create case)
+export interface ICaseItemType {
+	id: string
+	title: string
+	content: string
+	percent: number
+	picUrl: string
+	name: string
+	price: string
+	type: ImageBgType
 }
