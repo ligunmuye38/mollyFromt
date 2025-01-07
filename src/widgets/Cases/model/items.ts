2cd7b4d0a55d14610b0f6b788b88d86c35df8326
ligunmuye38 <ligun0805@hotmail.com>
2025-01-08 00:25:12 +0900

    add items for Cases

---
+++ src/widgets/Cases/model/items.ts
@@ -0,0 +1,123 @@
+import { ICaseType, IChance, ISelectImage, IType } from './types'
+
+export const caseType: IType[] = [
+	{
+		id: 1,
+		type: 'Classic'
+	},
+	{
+		id: 2,
+		type: 'Classic'
+	},
+	{
+		id: 3,
+		type: 'Classic'
+	},
+	{
+		id: 4,
+		type: 'Classic'
+	}
+]
+
+export const selectImages: ISelectImage[] = [
+	{
+		id: 'pic1',
+		picUrl: '/images/case/case-box.png'
+	},
+	{
+		id: 'pic2',
+		picUrl: '/images/case/case-box.png'
+	},
+	{
+		id: 'pic3',
+		picUrl: '/images/case/case-box.png'
+	},
+	{
+		id: 'pic4',
+		picUrl: '/images/case/case-box.png'
+	},
+	{
+		id: 'pic5',
+		picUrl: '/images/case/case-box.png'
+	},
+	{
+		id: 'pic6',
+		picUrl: '/images/case/case-box.png'
+	},
+	{
+		id: 'pic7',
+		picUrl: '/images/case/case-box.png'
+	}
+]
+
+export const chances: IChance[] = [
+	{
+		id: 'chance1',
+		name: 'P2000',
+		content: 'Blue Laminate',
+		price: 13.55,
+		percent: 14,
+		add_price: 400.33,
+		picUrl: '/images/livefeed/skin-8.png',
+		imageType: 'yello'
+	},
+	{
+		id: 'chance2',
+		name: 'P2000',
+		content: 'Blue Laminate',
+		price: 13.55,
+		percent: 14,
+		add_price: 400.33,
+		picUrl: '/images/livefeed/skin-8.png',
+		imageType: 'yello'
+	},
+	{
+		id: 'chance3',
+		name: 'P2000',
+		content: 'Blue Laminate',
+		price: 13.55,
+		percent: 14,
+		add_price: 400.33,
+		picUrl: '/images/livefeed/skin-8.png',
+		imageType: 'yello'
+	},
+	{
+		id: 'chance4',
+		name: 'P2000',
+		content: 'Blue Laminate',
+		price: 13.55,
+		percent: 14,
+		add_price: 400.33,
+		picUrl: '/images/livefeed/skin-8.png',
+		imageType: 'yello'
+	},
+	{
+		id: 'chance5',
+		name: 'P2000',
+		content: 'Blue Laminate',
+		price: 13.55,
+		percent: 14,
+		add_price: 400.33,
+		picUrl: '/images/livefeed/skin-8.png',
+		imageType: 'yello'
+	}
+]
+
+export const caseBodytype: ICaseType[] = [
+	{
+		id: 1,
+		type: 'All'
+	},
+	{
+		id: 2,
+		type: 'All'
+	},
+	{
+		id: 3,
+		type: 'All'
+	},
+	{
+		id: 4,
+		type: 'All'
+	}
+]
