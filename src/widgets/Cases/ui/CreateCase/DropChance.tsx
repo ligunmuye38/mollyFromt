f5981f46e233113f5f8fc0ac39bc552bfd2d666a
ligunmuye38 <ligun0805@hotmail.com>
2025-01-08 04:36:49 +0900

    update DropChances component for responsive

--- src/widgets/Cases/ui/CreateCase/DropChance.tsx
+++ src/widgets/Cases/ui/CreateCase/DropChance.tsx
@@ -3,6 +3,10 @@ import cls from "../Cases.module.sass";
 import Image from "next/image";
 
 const DropChance = () => {
+
+
+
+
     return (
         <div className={clsx(cls.chance_item)}>
             <div className={clsx(cls.chance_item_inner, "p-[10px] flex gap-[10px]")}>
@@ -23,18 +27,19 @@ const DropChance = () => {
                         </div>
                     </div>
                     <div className="flex gap-2 w-full">
-                        <div className={clsx(cls.chance_item_btn, "w-[90px]")}>
-                            <div className={clsx(cls.chance_item_btn_inner, "p-1.5 flex items-center")}>
-                                <div className="bg-[#17E2A5] rounded-md text-[#141925] text-[14px] font-[900] py-[5px] px-[9px]">
+                        <div className={clsx(cls.chance_item_btn, "w-[90px] sm:w-auto")}>
+                            <div className={clsx(cls.chance_item_btn_inner, "p-1.5 flex items-center sm:py-2.5 sm:px-2")}>
+                                <div className="bg-[#17E2A5] rounded-md text-[#141925] text-[14px] font-[900] py-[5px] px-[9px] sm:hidden flex">
                                     <div>%</div>
                                 </div>
-                                <div className="flex-1 flex justify-center items-center">
+                                <div className="flex-1 flex justify-center items-center sm:gap-1">
+                                    <span className="text-white text-[14px] font-[700] hidden sm:flex">%</span>
                                     <span className="text-white text-[14px] font-[700]">14</span>
                                 </div>
                             </div>
                         </div>
                         <div className={clsx(cls.chance_item_active, "flex flex-1")}>
-                            <div className={clsx(cls.chance_item_active_inner, "w-full flex justify-center items-center")}>
+                            <div className={clsx(cls.chance_item_active_inner, "w-full flex justify-center py-2.5 items-center")}>
                                 <div className="flex gap-0.5">
                                     <span className="text-white text-[14px] font-[700]">
                                         +
