f44bf296c670f567a722fc7115dad11326f9d591
ligunmuye38 <ligun0805@hotmail.com>
2025-01-08 04:33:23 +0900

    add case items data for the modal to create case

--- src/widgets/Cases/model/items.ts
+++ src/widgets/Cases/model/items.ts
@@ -1,4 +1,4 @@
-import { ICaseType, IChance, ISelectImage, IType } from './types'
+import { ICaseItemType, ICaseType, IChance, ISelectImage, IType } from './types'
 
 export const caseType: IType[] = [
 	{
@@ -59,7 +59,7 @@ export const chances: IChance[] = [
 		percent: 14,
 		add_price: 400.33,
 		picUrl: '/images/livefeed/skin-8.png',
-		imageType: 'yello'
+		imageType: 'yellow'
 	},
 	{
 		id: 'chance2',
@@ -69,7 +69,7 @@ export const chances: IChance[] = [
 		percent: 14,
 		add_price: 400.33,
 		picUrl: '/images/livefeed/skin-8.png',
-		imageType: 'yello'
+		imageType: 'yellow'
 	},
 	{
 		id: 'chance3',
@@ -79,7 +79,7 @@ export const chances: IChance[] = [
 		percent: 14,
 		add_price: 400.33,
 		picUrl: '/images/livefeed/skin-8.png',
-		imageType: 'yello'
+		imageType: 'yellow'
 	},
 	{
 		id: 'chance4',
@@ -89,7 +89,7 @@ export const chances: IChance[] = [
 		percent: 14,
 		add_price: 400.33,
 		picUrl: '/images/livefeed/skin-8.png',
-		imageType: 'yello'
+		imageType: 'yellow'
 	},
 	{
 		id: 'chance5',
@@ -99,7 +99,7 @@ export const chances: IChance[] = [
 		percent: 14,
 		add_price: 400.33,
 		picUrl: '/images/livefeed/skin-8.png',
-		imageType: 'yello'
+		imageType: 'yellow'
 	}
 ]
 
@@ -119,5 +119,208 @@ export const caseBodytype: ICaseType[] = [
 	{
 		id: 4,
 		type: 'All'
+	}
+]
+
+export const caseItems: ICaseItemType[] = [
+	{
+		id: 'case-1',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-1.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'yellow'
+	},
+	{
+		id: 'case-2',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-2.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'pink'
+	},
+	{
+		id: 'case-3',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-3.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'purple'
+	},
+	{
+		id: 'case-4',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-4.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'blue'
+	},
+	{
+		id: 'case-5',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-5.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'limit_blue'
+	},
+	{
+		id: 'case-6',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-1.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'yellow'
+	},
+	{
+		id: 'case-7',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-2.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'pink'
+	},
+	{
+		id: 'case-8',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-3.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'purple'
+	},
+	{
+		id: 'case-9',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-4.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'blue'
+	},
+	{
+		id: 'case-10',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-5.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'limit_blue'
+	},
+	{
+		id: 'case-11',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-1.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'yellow'
+	},
+	{
+		id: 'case-12',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-2.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'pink'
+	},
+	{
+		id: 'case-13',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-3.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'purple'
+	},
+	{
+		id: 'case-14',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-4.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'blue'
+	},
+	{
+		id: 'case-15',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-5.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'limit_blue'
+	},
+	{
+		id: 'case-16',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-1.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'yellow'
+	},
+	{
+		id: 'case-17',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-2.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'pink'
+	},
+	{
+		id: 'case-18',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-3.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'purple'
+	},
+	{
+		id: 'case-19',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-4.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'blue'
+	},
+	{
+		id: 'case-20',
+		title: 'FT',
+		content: 'CHANCE',
+		percent: 0.064,
+		picUrl: '/images/case/skin-5.png',
+		name: 'AK-47 | Fire Serpent',
+		price: '456,05',
+		type: 'limit_blue'
 	}
 ]
