d2ba5de74ce1841ced360e5494d372228201a399
ligunmuye38 <ligun0805@hotmail.com>
2025-01-08 00:22:43 +0900

    create CaseItem component(for the modal to create case)

---
+++ src/entities/CaseItem/ui/CaseItem.tsx
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+import cls from "./CaseItem.module.sass";
+
+
+const CaseItem = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CaseItem;
