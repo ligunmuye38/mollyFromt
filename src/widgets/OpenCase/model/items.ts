d7b24e3010685e723fcb8e3b9701ec95bff63721
ligunmuye38 <ligun0805@hotmail.com>
2025-01-09 03:11:09 -0800

    add OpenCase items

---
+++ src/widgets/OpenCase/model/items.ts
@@ -0,0 +1,381 @@
+
+import { RarityTheme } from '@/shared/const/rarity'
+
+import { LiveFeedItem, ICaseItemType, IPagination } from './types'
+
+export const paginationData: IPagination[] = [
+    {
+        value: 1,
+        content: "X1"
+    },
+    {
+        value: 2,
+        content: "X2"
+    },
+    {
+        value: 3,
+        content: "X3"
+    },
+    {
+        value: 4,
+        content: "X4"
+    },
+    {
+        value: 5,
+        content: "X5"
+    },
+    {
+        value: 10,
+        content: "X10"
+    }
+]
+
+export const itemsList: LiveFeedItem[] = [
+    {
+        id: '1',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.RARE,
+        picUrl: '/images/livefeed/skin-1.png'
+    },
+    {
+        id: '2',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.MYTHICAL,
+        picUrl: '/images/livefeed/skin-2.png'
+    },
+    {
+        id: '3',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.COMMON,
+        picUrl: '/images/livefeed/skin-3.png'
+    },
+    {
+        id: '4',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.UNCOMMON,
+        picUrl: '/images/livefeed/skin-4.png'
+    },
+    {
+        id: '5',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.LEGENDARY,
+        picUrl: '/images/livefeed/skin-5.png'
+    },
+    {
+        id: '6',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.EXCEEDINGLY,
+        picUrl: '/images/livefeed/skin-6.png'
+    },
+    {
+        id: '7',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.ANCIENT,
+        picUrl: '/images/livefeed/skin-7.png'
+    },
+    {
+        id: '8',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.RARE,
+        picUrl: '/images/livefeed/skin-7.png'
+    },
+    {
+        id: '9',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.RARE,
+        picUrl: '/images/livefeed/skin-1.png'
+    },
+    {
+        id: '10',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.MYTHICAL,
+        picUrl: '/images/livefeed/skin-2.png'
+    },
+    {
+        id: '11',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.COMMON,
+        picUrl: '/images/livefeed/skin-3.png'
+    },
+    {
+        id: '12',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.UNCOMMON,
+        picUrl: '/images/livefeed/skin-4.png'
+    },
+    {
+        id: '13',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.LEGENDARY,
+        picUrl: '/images/livefeed/skin-5.png'
+    },
+    {
+        id: '14',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.EXCEEDINGLY,
+        picUrl: '/images/livefeed/skin-6.png'
+    },
+    {
+        id: '15',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.RARE,
+        picUrl: '/images/livefeed/skin-7.png'
+    },
+    {
+        id: '16',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.UNCOMMON,
+        picUrl: '/images/livefeed/skin-4.png'
+    },
+    {
+        id: '17',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.LEGENDARY,
+        picUrl: '/images/livefeed/skin-5.png'
+    },
+    {
+        id: '18',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.EXCEEDINGLY,
+        picUrl: '/images/livefeed/skin-6.png'
+    },
+    {
+        id: '19',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.RARE,
+        picUrl: '/images/livefeed/skin-7.png'
+    },
+    {
+        id: '20',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.RARE,
+        picUrl: '/images/livefeed/skin-7.png'
+    },
+    {
+        id: '21',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.UNCOMMON,
+        picUrl: '/images/livefeed/skin-4.png'
+    },
+    {
+        id: '22',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.LEGENDARY,
+        picUrl: '/images/livefeed/skin-5.png'
+    },
+    {
+        id: '23',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.EXCEEDINGLY,
+        picUrl: '/images/livefeed/skin-6.png'
+    },
+    {
+        id: '24',
+        title: 'Leaded Glass',
+        rarity: RarityTheme.RARE,
+        picUrl: '/images/livefeed/skin-7.png'
+    }
+]
+
+export const caseItems: ICaseItemType[] = [
+    {
+        id: 'case-1',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-1.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'yellow'
+    },
+    {
+        id: 'case-2',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-2.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'pink'
+    },
+    {
+        id: 'case-3',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-3.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'purple'
+    },
+    {
+        id: 'case-4',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-4.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'blue'
+    },
+    {
+        id: 'case-5',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-5.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'limit_blue'
+    },
+    {
+        id: 'case-6',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-1.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'yellow'
+    },
+    {
+        id: 'case-7',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-2.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'pink'
+    },
+    {
+        id: 'case-8',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-3.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'purple'
+    },
+    {
+        id: 'case-9',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-4.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'blue'
+    },
+    {
+        id: 'case-10',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-5.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'limit_blue'
+    },
+    {
+        id: 'case-11',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-1.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'yellow'
+    },
+    {
+        id: 'case-12',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-2.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'pink'
+    },
+    {
+        id: 'case-13',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-3.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'purple'
+    },
+    {
+        id: 'case-14',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-4.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'blue'
+    },
+    {
+        id: 'case-15',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-5.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'limit_blue'
+    },
+    {
+        id: 'case-16',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-1.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'yellow'
+    },
+    {
+        id: 'case-17',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-2.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'pink'
+    },
+    {
+        id: 'case-18',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-3.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'purple'
+    },
+    {
+        id: 'case-19',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-4.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'blue'
+    },
+    {
+        id: 'case-20',
+        title: 'FT',
+        content: 'CHANCE',
+        percent: 0.064,
+        picUrl: '/images/case/skin-5.png',
+        name: 'AK-47 | Fire Serpent',
+        price: '456,05',
+        type: 'limit_blue'
+    }
+]
