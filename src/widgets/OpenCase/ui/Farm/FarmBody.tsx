55de56f49e3e73b6e791d0922bc65989d1e1c96a
ligunmuye38 <ligun0805@hotmail.com>
2025-01-10 04:04:14 -0800

    add openFarm section in FarmBody component

--- src/widgets/OpenCase/ui/Farm/FarmBody.tsx
+++ src/widgets/OpenCase/ui/Farm/FarmBody.tsx
@@ -12,6 +12,7 @@ import { useCommonStore } from '@/entities/Common/model/store'
 import { useTranslations } from 'next-intl'
 
 import IconHint from '@/shared/assets/icons/icon-hint.svg'
+import OpenFarm from './OpenFarm'
 
 export const FarmBody = () => {
 
@@ -19,12 +20,9 @@ export const FarmBody = () => {
     const t = useTranslations();
 
     const setItemPosition = useCommonStore(state => state.setItemPosition)
-    const caseCount = useCommonStore(state => state.caseCount)
     const setCaseCount = useCommonStore(state => state.setCaseCount)
 
 
-    // For case count
-    const [caseCountState, setCaseCountState] = useState<number>(0);
 
     // Init function
     useEffect(() => {
@@ -34,51 +32,60 @@ export const FarmBody = () => {
     }, [])
 
 
-    // Case count hook
-    useEffect(() => {
-        setCaseCountState(caseCount);
-    }, [caseCount])
+    // Open farm state
+    const openFarm = useCommonStore(state => state.openFarm)
 
     return (
         <div className={clsx(cls.base)}>
             <div className={clsx(cls.container, '!pt-0')}>
-                <div className={cls.type}>{t('open_case.selected_top')}</div>
-                <div
-                    className={cls.inner}
-                    style={{ backgroundImage: `url(${openCaseBg.src})` }}
-                >
-                    <Backshape className={cls.backshape} />
-                    <div className={clsx(cls.backshape, '3sm:!left-0 3sm:!transform-none 3sm:!top-0', caseCountState == 0 ? "flex" : "hidden")}>
-                        <Image
-                            src={'/images/case/case-full-2.png'}
-                            alt='case'
-                            width={320}
-                            height={320}
-                        />
-                    </div>
-                    <div className={clsx(cls.grid, 'justify-end w-full flex-row ', caseCountState == 0 ? "flex" : "hidden")}>
-                        <div className={clsx(cls.farm_hint, ' max-w-[374px] mr-5 sm:mr-0 sm:mx-1 sm:mt-[140px]')}>
-                            <div className={clsx(cls.farm_hint_inner, 'p-4 flex gap-2')}>
-                                <IconHint className='w-7 h-7 fill-[#FFA3A3]' />
-                                <div className='flex flex-col gap-2'>
-                                    <span className='text-[#FFA3A3] text-[14px] font-[500] uppercase'>{t('case_farm.hint_title')}</span>
-                                    <span className='text-[#FFA3A3] text-[12px] flex flex-col'>
-                                        <span>{t('case_farm.hint_content1')}</span>
-                                        <span>{t('case_farm.hint_content2')}</span>
-                                    </span>
+                <div className={clsx(cls.type, openFarm == false ? "hidden" : "flex justify-center")}>{t('open_case.selected_top')}</div>
+                {
+                    openFarm == true && 
+                    <div
+                        className={cls.inner}
+                        style={{ backgroundImage: `url(${openCaseBg.src})` }}
+                    >
+                        <Backshape className={cls.backshape} />
+                        <div className={clsx(cls.backshape, '3sm:!left-0 3sm:!transform-none 3sm:!top-0 flex')}>
+                            <Image
+                                src={'/images/case/case-full-2.png'}
+                                alt='case'
+                                width={320}
+                                height={320}
+                            />
+                        </div>
+                        <div className={clsx(cls.grid, 'justify-end w-full flex-row flex')}>
+                            <div className={clsx(cls.farm_hint, ' max-w-[374px] mr-5 sm:mr-0 sm:mx-1 sm:mt-[140px]')}>
+                                <div className={clsx(cls.farm_hint_inner, 'p-4 flex gap-2')}>
+                                    <IconHint className='w-7 h-7 fill-[#FFA3A3]' />
+                                    <div className='flex flex-col gap-2'>
+                                        <span className='text-[#FFA3A3] text-[14px] font-[500] uppercase'>{t('case_farm.hint_title')}</span>
+                                        <span className='text-[#FFA3A3] text-[12px] flex flex-col'>
+                                            <span>{t('case_farm.hint_content1')}</span>
+                                            <span>{t('case_farm.hint_content2')}</span>
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
-
-                </div>
+                }
+                {
+                    openFarm == false && 
+                    <div
+                        className={cls.inner}
+                        style={{ backgroundImage: `url(${openCaseBg.src})` }}
+                    >
+                        <OpenFarm />
+                    </div>
+                }
+                
             </div>
-            <div className={clsx('relative w-full justify-center flex overflow-hidden')}>
+            <div className={clsx('relative w-full justify-center overflow-hidden', openFarm == false ? "hidden" : "flex")}>
                 <div className={clsx(cls.border_bg, 'h-0.5 bg-opacity-10 opacity-90 absolute w-full')}>
 
                 </div>
-                <div className={clsx(" border-[2px] w-2/5	 border-dotted")}>
+                <div className={clsx(" border-[2px] w-2/5 border-dotted")}>
 
                 </div>
             </div>
