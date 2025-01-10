b9abfeadff1ed9751c13d4708aebe82f000cc1f9
ligunmuye38 <ligun0805@hotmail.com>
2025-01-10 05:05:56 -0800

    update Support button in FaqHeader component, add goSupport function

--- src/widgets/Faq/ui/FaqHeader.tsx
+++ src/widgets/Faq/ui/FaqHeader.tsx
@@ -10,6 +10,7 @@ import IconSupportBtn from '@/shared/assets/icons/icon-support-btn.svg'
 import IconQuestion from '@/shared/assets/icons/icon-question.svg'
 import Button from '@/shared/ui/Button/Button';
 import IntroBar from '@/shared/ui/IntroBar/IntroBar';
+import { useRouter } from 'next/navigation';
 
 const FaqHeader = () => {
 
@@ -17,26 +18,37 @@ const FaqHeader = () => {
 
     const setItemPosition = useCommonStore(state => state.setItemPosition)
 
+    // For router
+    const router = useRouter();
+
     useEffect(() => {
         setItemPosition("left")
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    // Go support function
+
+    const goSupport = () => {
+        router.push('/support');
+    }
+
     return (
         <IntroBar title={'sidebar.faq'} content={'faq_bar.title'}
             icon={<IconFaq />}
             buttons={
-                <Button disableAnimation classNames={{ base: clsx('w-[140px] h-[45px] flex items-center relative', cls.fnav_item), content: clsx('w-full h-full flex justify-center items-center') }}>
-                    <div className='w-full h-full top-0 right-0 absolute'>
-                        <IconSupportBtn />
+                <div className={cls.button_bar}>
+                    <div className={cls.button_bar_inner}>
+                        <Button onPress={goSupport} classNames={{ base: clsx('w-[140px] h-[45px] flex items-center relative', cls.fnav_item), content: clsx('w-full h-full flex justify-center items-center') }}>
+                            <div className='flex gap-3 items-center px-2 justify-center w-full h-full z-[40] '>
+                                <div className={clsx('w-full h-[30px]', cls.fnav_item_icon)}>
+                                    <IconQuestion />
+                                </div>
+                                <span className={clsx('font-[700]  text-[#D1D9EB] text-[12px] font-[Gotham Medium]')}>{t('pages.support')}</span>
+                            </div>
+                        </Button>
                     </div>
-                    <div className='flex gap-3 items-center px-2 justify-center w-full h-full z-[40] '>
-                        <div className={clsx('w-full h-[30px]', cls.fnav_item_icon)}>
-                            <IconQuestion />
-                        </div>
-                        <span className={clsx('font-[700]  text-[#D1D9EB] text-[12px] font-[Gotham Medium]')}>{t('pages.support')}</span>
-                    </div>
-                </Button>
+                </div>
+               
             }
         />
     )
