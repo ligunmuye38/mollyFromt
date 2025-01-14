2c65481b13d950a78dd90f760ccaca14a15d0447
ligunmuye38 <ligun0805@hotmail.com>
2025-01-14 13:44:37 -0800

    update CaseLiveFeed component(remove shadow, etc)

--- src/widgets/OpenCase/ui/CaseLiveFeed.tsx
+++ src/widgets/OpenCase/ui/CaseLiveFeed.tsx
@@ -24,24 +24,24 @@ const CaseLiveFeed = () => {
     ))
 
     return (
-        <div className="px-5 w-full h-auto relative flex flex-col mt-2">
-            <div className={clsx(cls.livefeed_logo, "-mb-2")}>
-                <div className={clsx(cls.livefeed_logo_inner, "flex justify-center gap-1 pt-2.5")}>
-                    <IconCrown className={clsx("w-3 h-3", cls.crown_bg)} />
-                    <span className="text-[8px] text-[#D1D9EB] uppercase">{t('open_case.top_drops')}</span>
+        <div className="w-full overflow-hidden">
+            <div className="px-5 w-full h-auto relative flex flex-col mt-2">
+                <div className={clsx(cls.livefeed_logo, "-mb-[2px] z-[1]")}>
+                    <div className={clsx(cls.livefeed_logo_inner, "flex justify-center gap-1 pt-2.5")}>
+                        <IconCrown className={clsx("w-3 h-3", cls.crown_bg)} />
+                        <span className="text-[8px] text-[#D1D9EB] uppercase">{t('open_case.top_drops')}</span>
+                    </div>
                 </div>
-            </div>
-            <div className={clsx(cls.livefeed_container)}>
-                <div className={clsx(cls.livefeed_container_inner)}>
-                    <div className="relative w-full h-full z-20 p-1 flex gap-1 overflow-hidden">
-                        {items}
-                        <div className={clsx(" absolute right-0 w-[84px] h-[78px]", cls.feed_shadow)}>
-
+                <div className={clsx(cls.livefeed_container)}>
+                    <div className={clsx(cls.livefeed_container_inner, 'overflow-hidden')}>
+                        <div className="relative w-full h-full z-20 p-1 flex gap-0.5">
+                            {items}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+       
         
     )
 }
