803de8a720f33ed004e4f6996d9646764c724a99
ligunmuye38 <ligun0805@hotmail.com>
2025-01-14 04:49:15 -0800

    update faq button in SupportHeader component

--- src/widgets/Support/ui/SupportHeader.tsx
+++ src/widgets/Support/ui/SupportHeader.tsx
@@ -43,28 +43,35 @@ const SupportHeader = () => {
     return (
         <IntroBar title={'sidebar.support'} icon={<IconQuestion />} content={'support_datas.title'}
             buttons={
-                <div className="flex gap-3 flex-row md:flex-col h-full items-center">
+                <div className="flex gap-3 flex-row 3sm:flex-col h-full items-center">
                     <Button onPress={createNewTicket} disableAnimation classNames={{ base: clsx('w-[170px] md:w-[140px] h-[45px] flex items-center relative'), content: clsx('w-full h-full flex justify-center items-center') }}>
                         <div className='w-full h-full top-0 right-0 absolute'>
                             <IconNewTicket className={'w-full h-full'} />
                         </div>
                         <div className='flex gap-3 items-center px-2 justify-center w-full h-full z-[40] '>
                             <span className={clsx("text-[#FDCD24] text-[26px]")}>+</span>
-                            <span className={clsx('font-[700]  text-[#D1D9EB] text-[12px] font-[Gotham Medium] block md:hidden')}>{t('support_datas.new_ticket')}</span>
+                            <span className={clsx('font-[700]  text-[#D1D9EB] text-[12px] font-[Gotham Medium] block')}>{t('support_datas.new_ticket')}</span>
                         </div>
                     </Button>
-                    <div className={cls.button_bar}>
-                        <div className={cls.button_bar_inner}>
-                            <Button onPress={goFaq} classNames={{ base: clsx('w-[140px] h-[45px] flex items-center relative', cls.fnav_item), content: clsx('w-full h-full flex justify-center items-center') }}>
-                                <div className='flex gap-3 items-center px-2 justify-center w-full h-full z-[40] '>
-                                    <div className={clsx('w-full h-[30px]', cls.fnav_item_icon)}>
-                                        <IconQuestion />
-                                    </div>
-                                    <span className={clsx('font-[700]  text-[#D1D9EB] text-[12px] font-[Gotham Medium]')}>{t('pages.faq')}</span>
-                                </div>
-                            </Button>
-                        </div>
-                    </div>
+                    <Button
+                        theme='grey-4'
+                        strokeSize='reg'
+                        backdrop
+                        hexagon
+                        hexagonAxis='x'
+                        classNames={{
+                            base: 'h-10 2sm:hidden flex',
+                            content: 'px-6 text-xs'
+                        }}
+                        startContent={
+                            <div className={clsx('w-full h-[30px]', cls.fnav_item_icon)}>
+                                <IconQuestion />
+                            </div>
+                        }
+                        onClick={goFaq}
+                    >
+                        {t('pages.faq')}
+                    </Button>
                 </div>
             }
         />
