9e41b807fa3bdde70bbd867a603b763a7ab3ae63
ligunmuye38 <ligun0805@hotmail.com>
2025-01-14 04:49:42 -0800

    add filter button in TopCase component

--- src/widgets/TopCases/ui/TopCases.tsx
+++ src/widgets/TopCases/ui/TopCases.tsx
@@ -1,16 +1,20 @@
-// 'use client'
+'use client'
 import { caseList } from '../model/items'
 import clsx from 'clsx'
 import { useTranslations } from 'next-intl'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { CaseCard } from '@/entities/CaseCard/ui/CaseCard'
 
 import IconCase from '@/shared/assets/icons/icon-case-grey.svg'
 import Button from '@/shared/ui/Button/Button'
 import { SectionHeader } from '@/shared/ui/SectionHeader/SectionHeader'
+import IconFilter from '@/shared/assets/icons/icon-filter.svg'
 
 import cls from './TopCases.module.sass'
+import { useAppResponsive } from '@/shared/lib/useResponsive'
+import { CaseToolbarContent } from '@/entities/CaseToolbar/ui/CaseToolbarContent'
+
 
 interface TopCasesProps {
 	className?: string
@@ -18,6 +22,11 @@ interface TopCasesProps {
 
 export const TopCases: FC<TopCasesProps> = ({ className }) => {
 	const t = useTranslations()
+
+	const breakpoints = useAppResponsive()
+
+	const [isFilter, setIsFilter] = useState<boolean>(false);
+
 
 	const cases = caseList.map(item => (
 		<div
@@ -32,12 +41,13 @@ export const TopCases: FC<TopCasesProps> = ({ className }) => {
 	))
 
 	return (
-		<div className={clsx(className)}>
+		<div className={clsx(className, 'relative')}>
 			<SectionHeader
 				title={t('topcases.title')}
 				icon={<IconCase />}
 				subtitle={t('topcases.subtitle')}
 				barElement={
+					breakpoints.lg ? 
 					<Button
 						theme='grey-4'
 						strokeSize='reg'
@@ -51,8 +61,21 @@ export const TopCases: FC<TopCasesProps> = ({ className }) => {
 					>
 						See more
 					</Button>
+					:
+					<Button
+						hexagon
+						hexagonAxis='y'
+						disableAnimation
+						startContent={<IconFilter />}
+						onClick={() => setIsFilter(!isFilter)}
+					>
+					</Button>
 				}
 			/>
+			<div className={clsx('absolute top-12 z-[20]', isFilter == true ? "flex" : "hidden")}>
+				<CaseToolbarContent className='scrollbar-hide py-1 px-0.5 bg-[#141925] border-[#161C28] border-[2px] rounded-[12px]' />
+			</div>
+			
 			<div className={clsx(cls.grid, 'mt-6')}>{cases}</div>
 		</div>
 	)
