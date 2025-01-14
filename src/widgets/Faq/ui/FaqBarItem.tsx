23d95cc05641054626432ba036529ccb54bda129
ligunmuye38 <ligun0805@hotmail.com>
2025-01-14 13:43:39 -0800

    update FaqBarItem height for sm(47px)

--- src/widgets/Faq/ui/FaqBarItem.tsx
+++ src/widgets/Faq/ui/FaqBarItem.tsx
@@ -9,7 +9,7 @@ const FaqBarItem = ({ content, icon: Icons }: FaqBarItemType) => {
     const t = useTranslations();
 
     return (
-        <div className={clsx('w-full h-[94px]', cls.faq, cls.item)}>
+        <div className={clsx('w-full h-[94px] 2sm:h-[47px]', cls.faq, cls.item)}>
             <div className={clsx('w-full h-full', cls.faq_border, cls.border_12)}>
                 <div className={clsx(cls.faq_body, cls.border_12, 'flex w-full justify-center items-center gap-3 h-full')}>
                     <div className={clsx('w-8 h-8', cls.item_icon)}>
