cbaa2fdaa5163561eae1c4c8bb2f749983087278
ligunmuye38 <ligun0805@hotmail.com>
2025-01-14 04:36:06 -0800

    add IconFile state, icon in ChatBar component

--- src/widgets/Chat/ui/ChatBar.tsx
+++ src/widgets/Chat/ui/ChatBar.tsx
@@ -4,15 +4,17 @@ import { FC } from 'react'
 
 import IconSend from '@/shared/assets/icons/icon-send.svg'
 import IconSmile from '@/shared/assets/icons/icon-smile.svg'
+import IconFile from '@/shared/assets/icons/icon-select-file.svg'
 import Button from '@/shared/ui/Button/Button'
 
 import cls from './Chat.module.sass'
 
 interface ChatBarProps {
 	className?: string
+	isEmoji?: boolean
 }
 
-export const ChatBar: FC<ChatBarProps> = ({ className }) => {
+export const ChatBar: FC<ChatBarProps> = ({ className, isEmoji = true }) => {
 	const t = useTranslations()
 	return (
 		<div className={clsx(cls.bar, className)}>
@@ -31,7 +33,12 @@ export const ChatBar: FC<ChatBarProps> = ({ className }) => {
 						base: cls.bar_btn_picker
 					}}
 				>
-					<IconSmile className={cls.bar_btn_picker_icon} />
+					{
+						isEmoji == true ? 
+							<IconSmile className={cls.bar_btn_picker_icon} />
+							:
+							<IconFile className={cls.bar_btn_picker_icon} />
+					}
 				</Button>
 				<Button
 					theme='gradient-outline-green-3'
