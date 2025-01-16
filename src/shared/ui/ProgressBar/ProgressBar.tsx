7cee00a45bddf3179b95ed29b5a32a8e9a9eaeb2
ligunmuye38 <ligun0805@hotmail.com>
2025-01-16 00:28:05 -0800

    add ProgressBar component

--- src/shared/ui/ProgressBar/ProgressBar.tsx
+++ src/shared/ui/ProgressBar/ProgressBar.tsx
@@ -1,28 +1,18 @@
+
+import clsx from "clsx";
+import cls from './ProgressBar.module.sass'
+
 const ProgressBar = () => {
     return (
-        <div x-data="skillDisplay"
-        className="p-6 space-y-6 bg-gray-800 rounded-xl md:grid md:grid-cols-2 md:gap-4 sm:space-y-0">
-        <div className="grid grid-cols-2 gap-6">
-            <template x-for="skill in skills">
-                <button x-text="skill.title"
-                    className="px-4 py-2 text-xl text-gray-100 transition bg-blue-600 rounded-md h-14 w-44 hover:bg-blue-700"
-                    :className="(currentSkill.title == skill.title) && 'font-bold ring-2 ring-gray-100'"
-                    @click="currentSkill = skill"></button>
-            </template>
+        <div className="w-full rounded-full h-2.5 bg-[#2A3246] relative">
+            <div
+                className={clsx("h-2.5 rounded-full", cls.progress_color)}
+                style={{ width: `${30}%` }}
+            ></div>
+            <div className={clsx(" absolute w-5 h-5 rounded-large p-[5px]", cls.pointer)} style={{left: '30%'}}>
+                <div className="w-full h-full rounded-large bg-[#0A7455]"></div>
+            </div>
         </div>
-
-        <div className="flex items-center justify-center" x-data="{ circumference: 2 * 22 / 7 * 120 }">
-            <svg className="transform -rotate-90 w-72 h-72">
-                <circle cx="145" cy="145" r="120" stroke="currentColor" stroke-width="30" fill="transparent"
-                    className="text-gray-700" />
-
-                <circle cx="145" cy="145" r="120" stroke="currentColor" stroke-width="30" fill="transparent"
-                    :stroke-dasharray="circumference"
-                    :stroke-dashoffset="circumference - currentSkill.percent / 100 * circumference"
-                    className="text-blue-500 " />
-            </svg>
-            <span className="absolute text-5xl" x-text="`${currentSkill.percent}%`"></span>
-    </div>
     )
 }
 
