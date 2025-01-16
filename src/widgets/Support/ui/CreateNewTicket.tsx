006daa904bc3658c1a80b4cc1b197fa73afc7208
ligunmuye38 <ligun0805@hotmail.com>
2025-01-16 05:55:57 -0800

    add create ticket button shadow in CreateNewTicket component

--- src/widgets/Support/ui/CreateNewTicket.tsx
+++ src/widgets/Support/ui/CreateNewTicket.tsx
@@ -43,23 +43,24 @@ const CreateNewTicket = () => {
                 <span className="text-[#545778] font-[500] text-[15px]">{t('create_new_ticket.describe')}</span>
                 <textarea className={clsx("bg-[#0D1018] border-0 rounded-lg min-h-[200px] max-h-[200px] app-scrollbar", cls.bar_input)} />
             </div>
-            <div className="flex justify-end h-auto items-center gap-3">
+            <div className="flex justify-end h-auto items-center gap-3 2sm:flex-col">
                 <div className="flex gap-3 w-auto h-full items-center">
                     <div className="text-white w-auto text-[14px] font-[500]">{t('create_new_ticket.max_size')}</div>
                     <IconUploadFile className="fill-[#545778] w-5 h-5" />
                 </div>
-                <div className={clsx(cls.create_ticket, 'w-[168px] h-[38px]')}>
-                    <div className={cls.create_ticket_inner}>
-                        <Button
-                            classNames={{
-                                base: 'relative w-full h-full',
-                            }}
-                        >
-                            {t('create_new_ticket.create_ticket').toUpperCase()}
-                        </Button>
+                <div className={clsx(cls.button_shadow, 'h-[45px] w-auto z-10 flex justify-center items-center')}>
+                    <div className={clsx(cls.create_ticket, 'w-[168px] h-[38px] z-[10]')}>
+                        <div className={cls.create_ticket_inner}>
+                            <Button
+                                classNames={{
+                                    base: 'relative w-full h-full',
+                                }}
+                            >
+                                {t('create_new_ticket.create_ticket').toUpperCase()}
+                            </Button>
+                        </div>
                     </div>
                 </div>
-                
             </div>
             <div className=" rounded-lg border-[#1B2130] border-[1px] p-2 w-full h-auto relative text-wrap break-words">
                 <div className="text-[#545778] font-[13px] text-wrap h-full relative">zip,rar,svg,txt,png,jpeg,jpg,mp4,mp3,pdf,docx,doc,xlsx,xls,webp</div>
