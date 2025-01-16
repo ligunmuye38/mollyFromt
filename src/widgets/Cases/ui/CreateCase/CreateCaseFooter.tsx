4eb9452952b2bb523041ebf7566536586b10a7ae
ligunmuye38 <ligun0805@hotmail.com>
2025-01-16 00:31:21 -0800

    add ProgressBar, update create case button(remove css, use Button with hexagon property) in CreateCaseFooter component

--- src/widgets/Cases/ui/CreateCase/CreateCaseFooter.tsx
+++ src/widgets/Cases/ui/CreateCase/CreateCaseFooter.tsx
@@ -6,6 +6,7 @@ import cls from "../Cases.module.sass"
 
 import IconBagTick from "@/shared/assets/icons/icon-bag-tick.svg"
 import IconPlus from "@/shared/assets/icons/icon-black-plus.svg"
+import ProgressBar from "@/shared/ui/ProgressBar/ProgressBar";
 
 const CreateCaseFooter = () => {
     // For translation
@@ -14,8 +15,8 @@ const CreateCaseFooter = () => {
     return (
         <div className={clsx(cls.footer_body, "w-full")}>
             <div className={clsx(cls.footer_body_inner, "w-full h-full py-3 px-4")}>
-                <div className="flex justify-between items-center flex-row md:flex-col md:justify-center md:items-center md:gap-2">
-                    <div className={clsx(cls.footer_body_btn)}>
+                <div className="flex justify-between items-center flex-row md:flex-col md:justify-center md:items-center md:gap-4">
+                    <div className={clsx(cls.footer_body_btn, 'md:order-2')}>
                         <div className={clsx(cls.footer_body_btn_inner, "py-[10px] px-3 gap-2 flex items-center")}>
                             <IconBagTick className="w-6 h-6" />
                             <div className="flex gap-1">
@@ -27,16 +28,24 @@ const CreateCaseFooter = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex items-center">
-                        <Button className={clsx(cls.header_btn, "flex items-center mr-4 md:mr-0")}
+                    <div className="flex-1 px-4 h-full justify-center items-center gap-[5px] flex flex-col md:order-1 md:w-full">
+                        <span className="text-[9px] uppercase text-[#60719A] font-[700]">{t('cases_header.percent_of_income')}</span>
+                        <ProgressBar />
+                    </div>
+                    <div className={clsx(cls.shadow, 'py-[1px] !z-10 px-[1px] relative md:order-3')}>
+                        <Button
+                            theme='grey-4'
+                            strokeSize='reg'
+                            backdrop
+                            hexagon
+                            hexagonAxis='x'
                             classNames={{
-                                content: "w-full h-full"
+                                base: clsx('h-10 !z-[1]', cls.header_btn),
+                                content: clsx('px-6 text-xs', cls.header_btn_inner)
                             }}
+                            startContent={<IconPlus className="w-[20px] h-[20px]" />}
                         >
-                            <div className={clsx(cls.header_btn_inner, "flex justify-center items-center gap-2")}>
-                                <IconPlus className="w-[20px] h-[20px]" />
-                                <span className={clsx("font-[900] text-[14px] text-[#121722] uppercase", cls.header_btn_font)}>{t('cases_header.create_case')}</span>
-                            </div>
+                            <span className={clsx("font-[900] text-[14px] text-[#121722] uppercase", cls.header_btn_font)}>{t('cases_header.create_case')}</span>
                         </Button>
                     </div>
                 </div>
