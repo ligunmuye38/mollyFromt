8ea52a5408d1a0a6faef29306579eeb15d99a101
ligunmuye38 <ligun0805@hotmail.com>
2025-01-17 10:39:50 -0800

    update width based on current route in FixedTopBar component

--- src/widgets/FixedTopBar/ui/FixedTopBar.tsx
+++ src/widgets/FixedTopBar/ui/FixedTopBar.tsx
@@ -67,7 +67,7 @@ export const FixedTopBar: FC<FixedTopBarProps> = ({ className }) => {
 					right: window.innerWidth - (holderRef.current?.getBoundingClientRect().right ?? holderWidth),
 					maxWidth: holderWidth
 				}}
-				className={clsx('fixed top-4 z-[100] flex items-start justify-end gap-4', className, {
+				className={clsx('fixed top-4 z-[100] flex items-start justify-end gap-4', className, (pathname == AppRoute.MAIN || pathname == AppRoute.CASES) ? "w-full" : "!w-auto", {
 					['top-[-200px] duration-500']: headerBarInView,
 					['w-full']: fixedCaseToolbarInView
 				})}
