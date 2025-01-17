73cb4b208c581edd5d806407122eddad9074bade
ligunmuye38 <ligun0805@hotmail.com>
2025-01-17 10:37:36 -0800

    update ListItem component for active state

--- src/entities/ListItem/ui/ListItem.tsx
+++ src/entities/ListItem/ui/ListItem.tsx
@@ -41,19 +41,21 @@ const ListItme = ({icon: Icon, title, isActived, onClick}: ListItemProps) => {
                     </div>
                 </Button>
             </div>
-            <div className="hidden lg:flex w-full h-[59px]">
-                <div className={clsx('rounded-[10px] p-[1px] h-full w-full', isActived == true ? 'bg-[#17C993]' : 'bg-[#191F2D80]')}>
+            <div className="hidden lg:flex w-full">
+                <div className={clsx('rounded-[10px] p-[1px] min-h-[65px] max-h-[65px] lg:min-w-[82px] w-full', isActived == true ? 'bg-[#17C993]' : 'bg-[#191F2D80]')}>
                     <div className={clsx("w-full h-full rounded-[10px]", isActived == true ? 'bg-[#0A372A]' : 'bg-[#191F2D80]')}>
                         <Button
                             classNames={{
-                                base: "w-full h-full py-[8px] flex justify-between",
+                                base: "w-full h-full flex pt-[5px] justify-between",
                                 content: "w-full h-full flex !justify-center flex-col"
                             }}
                             onClick={clickHandle}
                         >
-                            <div className="flex gap-2 items-center flex-col">
-                                <Icon className={clsx("w-[24px] h-[25px]", isActived == false ? "fill-[#5A6786]" : "fill-[#19CF99]")} />
-                                <span className={clsx("text-[10px]", isActived == false ? "text-[#5A6786]" : "text-[#FFFFFF]")}>{title}</span>
+                            <div className="flex flex-col items-center h-full relative">
+                                <Icon className={clsx("md:w-[23px] md:h-[23px] lg:w-[25px] lg:h-[25px]", isActived == false ? "fill-[#5A6786]" : "fill-[#19CF99]")} />
+                                <div className="flex flex-1 h-full justify-center items-center">
+                                    <span className={clsx("md:text-[12px] 2sm:text-[10px] lg:text-[14px]", isActived == false ? "text-[#5A6786]" : "text-[#FFFFFF]")}>{title}</span>
+                                </div>
                             </div>
                         </Button>
                     </div>
