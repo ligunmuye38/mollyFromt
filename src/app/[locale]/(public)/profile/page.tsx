19e8ca45e6920e6506654e53c2349cb6acb48d18
ligunmuye38 <ligun0805@hotmail.com>
2025-01-17 10:35:47 -0800

    add Main tag, metadata in profile page

--- src/app/[locale]/(public)/profile/page.tsx
+++ src/app/[locale]/(public)/profile/page.tsx
@@ -1,6 +1,25 @@
+import { IMetadata } from '@/shared/types/metadata'
+
+import Main from "@/widgets/Profile/ui/Main/Main";
+
+export async function generateMetadata({ params: { locale } }: IMetadata) {
+    // const t = await getTranslations({ locale, namespace: 'Main' })
+
+    return {
+        title: {
+            default: 'Profile/Layout'
+        },
+        description: ''
+    }
+}
+
 const ProfilePage = () => {
     return (
-        <div></div>
+        <div className='w-full pr-4 lg:!p-0'>
+            <div className='w-full'>
+                <Main />
+            </div>
+        </div>
     )
 }
 
