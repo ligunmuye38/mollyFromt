e9a10d21aa45bcc8b6884a42ab9049fcef4f13d2
ligunmuye38 <ligun0805@hotmail.com>
2025-01-17 10:36:44 -0800

    add active state, control in CaseItem component

--- src/entities/CaseItem/ui/CaseItem.tsx
+++ src/entities/CaseItem/ui/CaseItem.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useTranslations } from "next-intl";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import clsx from "clsx";
 import cls from "./CaseItem.module.sass";
@@ -26,10 +26,11 @@ interface CaseItemProps {
     hoverContent?: React.ReactNode
     isOpen?: boolean
     isRotate?: boolean
+    isActive?: boolean
     onClick?: () => void
 }
 
-const CaseItem = ({ title, content, percent, picUrl, name, price, type, className, isHover = false, isRotate = true, hoverContent, isOpen = true, backTheme, onClick }: CaseItemProps) => {
+const CaseItem = ({ title, content, percent, picUrl, name, price, type, className, isHover = false, isRotate = true, hoverContent, isOpen = true, backTheme, isActive = false, onClick }: CaseItemProps) => {
 
 
     // For translation
@@ -81,6 +82,16 @@ const CaseItem = ({ title, content, percent, picUrl, name, price, type, classNam
         }
     }
 
+    const [isMobile, setIsMobile] = useState(false);
+
+    const isMobileDevice = () => /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    useEffect(() => {
+
+        setIsMobile(isMobileDevice().valueOf())
+        console.log(isMobileDevice().valueOf())
+    }, []);
+
 
     const [isHovered, setIsHovered] = useState<boolean>(false);
     const [isRotated, setIsRotated] = useState<boolean>(isRotate);
@@ -88,23 +99,29 @@ const CaseItem = ({ title, content, percent, picUrl, name, price, type, classNam
     const [isRotation, setIsRotation] = useState<boolean>(false)
 
     const hoverHandle = (value: boolean) => {
-        if (isHover == true && isOpened == true) {
+        if (isHover == true && isOpened == true && isMobile == false) {
             setIsHovered(value);
         }
     }
 
     // click function
     const clickHandle = () => {
-        if (isRotated == true) {
-            if (isOpened == false) {
-                setIsRotation(true)
-                setTimeout(() => {
-                    setIsOpened(true)
-                }, 150);
-            }
-            else if (isOpened == true) {
-                if (onClick) {
-                    onClick();
+        if (isMobile == true && isHover == true && isHovered == false) {
+            setIsHovered(true)
+        }
+        else {
+            setIsHovered(!isHovered)
+            if (isRotated == true) {
+                if (isOpened == false) {
+                    setIsRotation(true)
+                    setTimeout(() => {
+                        setIsOpened(true)
+                    }, 150);
+                }
+                else if (isOpened == true) {
+                    if (onClick) {
+                        onClick();
+                    }
                 }
             }
         }
@@ -113,40 +130,42 @@ const CaseItem = ({ title, content, percent, picUrl, name, price, type, classNam
     return (
         <div className={clsx("w-auto h-auto hover:cursor-pointer", isRotation && cls.open_rotation)}
             onClick={() => clickHandle()}
-            onMouseEnter={() => hoverHandle(true)} 
+            onMouseEnter={() => hoverHandle(true)}
             onMouseLeave={() => hoverHandle(false)}>
             <div className={clsx(cls.item_box, backTheme && cls[backTheme])}>
                 {
-                    isRotated == true && 
+                    isRotated == true &&
                     <>
                         {
                             isOpened == true &&
-                            <div className={clsx("w-[133px] h-[153px] rounded-[12px] p-[13px] flex flex-col justify-between relative overflow-hidden", cls.item_shadow, cls.item, className)}>
-                                <div className={clsx("flex items-start justify-between")}>
-                                    <span className="text-[#2F374A] font-[700] text-[10px] uppercase">{title}</span>
-                                    <div className="flex items-start gap-2">
-                                        <div className={clsx("flex flex-col justify-end items-end gap-[3px]")}>
-                                            <span className="text-[#2F374A] font-[500] text-[8px] uppercase">{content}</span>
-                                            <span className="text-[#10AA7C] text-[12px] font-[500]">{percent}%</span>
-                                        </div>
-                                        <div className={clsx("w-[8px] h-[8px] rounded-[2px] mt-1", getMarkBackground(type))}>
+                            <div className={clsx("w-[133px] h-[153px] rounded-[12px] p-[1px] relative overflow-hidden", isActive ? "bg-[#10AA7C]" : "bg-[#161C28]")}>
+                                <div className={clsx("w-full h-full rounded-[12px] p-[13px] flex flex-col justify-between relative overflow-hidden", cls.item_shadow, cls.item, className)}>
+                                    <div className={clsx("flex items-start justify-between")}>
+                                        <span className="text-[#2F374A] font-[700] text-[10px] uppercase">{title}</span>
+                                        <div className="flex items-start gap-2">
+                                            <div className={clsx("flex flex-col justify-end items-end gap-[3px]")}>
+                                                <span className="text-[#2F374A] font-[500] text-[8px] uppercase">{content}</span>
+                                                <span className="text-[#10AA7C] text-[12px] font-[500]">{percent}%</span>
+                                            </div>
+                                            <div className={clsx("w-[8px] h-[8px] rounded-[2px] mt-1", getMarkBackground(type))}>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className=" absolute w-[107px] h-[76px] top-[29px] left-[13px]">
-                                    <div className={clsx("relative w-full h-full flex justify-center items-center", getBackground(type))}>
-                                        <Image src={picUrl} height={59} width={80} alt="box" />
+                                    <div className=" absolute w-[107px] h-[76px] top-[29px] left-[13px]">
+                                        <div className={clsx("relative w-full h-full flex justify-center items-center", getBackground(type))}>
+                                            <Image src={picUrl} height={59} width={80} alt="box" />
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-col gap-1">
+                                        <span className="text-[#2F374A] text-[10px] font-[500]">{name}</span>
+                                        <span className="text-[#D1D9EB] text-[12px] font-[500]">$ {price}</span>
                                     </div>
                                 </div>
-                                <div className="flex flex-col gap-1">
-                                    <span className="text-[#2F374A] text-[10px] font-[500]">{name}</span>
-                                    <span className="text-[#D1D9EB] text-[12px] font-[500]">$ {price}</span>
-                                </div>
-
-                                    <div className={clsx("absolute w-full bg-[#1E2536A6] h-full left-0 z-[1] duration-400", cls.hover_blur, isHovered == true ? "top-0" : "top-full")}>
+                                <div className={clsx("absolute w-full bg-[#1E2536A6] h-full left-0 z-[1] duration-400", cls.hover_blur, isHovered == true ? "top-0" : "top-full")}>
                                     {hoverContent && hoverContent}
                                 </div>
                             </div>
+
                         }
                         {
                             isOpened == false &&
@@ -155,26 +174,26 @@ const CaseItem = ({ title, content, percent, picUrl, name, price, type, classNam
                             </div>
                         }
                     </>
-                    
+
                 }
                 {
-                    isRotated == false && 
+                    isRotated == false &&
                     <div className={clsx("w-[133px] h-[153px] rounded-[12px] p-[13px] flex flex-col justify-center items-center relative bg-[#141925]", cls.item_back)}>
-                            <div className={clsx(cls.item_open_hexagon)}>
-                                <div className={clsx(cls.item_open_hexagon_body)}>
-                                    <div className={clsx(cls.item_open_hexagon_inner, 'flex justify-center items-center')}>
-                                        <span className="text-[#516080] text-12px uppercase font-[700]">{t('open')}</span>
-                                    </div>
+                        <div className={clsx(cls.item_open_hexagon)}>
+                            <div className={clsx(cls.item_open_hexagon_body)}>
+                                <div className={clsx(cls.item_open_hexagon_inner, 'flex justify-center items-center')}>
+                                    <span className="text-[#516080] text-12px uppercase font-[700]">{t('open')}</span>
                                 </div>
-
                             </div>
+
                         </div>
+                    </div>
                 }
-            
+
             </div>
-            
+
         </div>
-        
+
     )
 }
 
