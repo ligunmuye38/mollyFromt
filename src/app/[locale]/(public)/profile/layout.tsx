cee90397996d135f6b90a61e408f348fab1eeb7e
ligunmuye38 <ligun0805@hotmail.com>
2025-01-17 10:35:10 -0800

    remove Metadata, update the layout.(when current route is user, the profilemain will disappear) for profile

--- src/app/[locale]/(public)/profile/layout.tsx
+++ src/app/[locale]/(public)/profile/layout.tsx
@@ -1,28 +1,28 @@
-import { IMetadata } from '@/shared/types/metadata'
+"use client"
+import { usePathname } from '@/shared/config/i18n/navigation';
 import ProfileHeader from '@/widgets/Profile/ui/ProfileHeader'
 import ProfileMain from '@/widgets/Profile/ui/ProfileMain'
 
-export async function generateMetadata({ params: { locale } }: IMetadata) {
-    // const t = await getTranslations({ locale, namespace: 'Main' })
 
-    return {
-        title: {
-            default: 'Profile/Layout'
-        },
-        description: ''
-    }
-}
 
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+
+    // To get current route
+    const pathname = usePathname();
+
     return (
         <div className='mt-5 flex-col flex'>
             <ProfileHeader />
             <div className='flex lg:flex-col'>
-                <div className='w-auto h-screen'>
-                    <ProfileMain />
-                </div>
-                <div className='flex-1 h-screen'>
+                {
+                    pathname != "/profile/user" &&
+                    <div className='w-auto h-screen lg:h-auto'>
+                        <ProfileMain />
+                    </div>
+                }
+
+                <div className='flex-1'>
                     {children}
                 </div>
             </div>
