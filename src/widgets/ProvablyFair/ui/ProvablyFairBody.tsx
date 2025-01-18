9b48f9d37fb3fe610345bb4c6480ac70c87b6325
ligunmuye38 <ligun0805@hotmail.com>
2025-01-18 08:38:46 -0800

    disable scroll function to move selected item in ProvablyFairBody component

--- src/widgets/ProvablyFair/ui/ProvablyFairBody.tsx
+++ src/widgets/ProvablyFair/ui/ProvablyFairBody.tsx
@@ -120,28 +120,28 @@ const ProvablyFairBody = () => {
 
 
     // Hook function
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    const id = entry.target.id;
-                    updateActiveState(id);
-                }
-            });
-        });
+    // useEffect(() => {
+    //     const observer = new IntersectionObserver((entries) => {
+    //         entries.forEach((entry) => {
+    //             if (entry.isIntersecting) {
+    //                 const id = entry.target.id;
+    //                 updateActiveState(id);
+    //             }
+    //         });
+    //     }, { rootMargin: '150px' });
 
-        // Observe each section
-        refs.current.forEach((ref) => {
-            if (ref) {
-                observer.observe(ref);
-            }
-        });
+    //     // Observe each section
+    //     refs.current.forEach((ref) => {
+    //         if (ref) {
+    //             observer.observe(ref);
+    //         }
+    //     });
 
-        return () => {
-            // Clean up the observer on component unmount
-            observer.disconnect();
-        };
-    }, [router]);
+    //     return () => {
+    //         // Clean up the observer on component unmount
+    //         observer.disconnect();
+    //     };
+    // }, [router]);
 
     return (
         <div className="h-auto w-full relative flex lg:flex-col flex-row px-5 lg:px-0">
