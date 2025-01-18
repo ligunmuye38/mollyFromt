e9147ca4f5f9982afba704768ce89fb8060fd7f2
ligunmuye38 <ligun0805@hotmail.com>
2025-01-18 08:09:15 -0800

    add onClick function in List, ListItem component

--- src/shared/ui/List/ListItem.tsx
+++ src/shared/ui/List/ListItem.tsx
@@ -1,6 +1,6 @@
 "use client"
 import clsx from "clsx";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 interface ListItemProps {
     classNames?: {
@@ -13,17 +13,18 @@ interface ListItemProps {
     startIcon?: React.ReactNode
     endIcon?: React.ReactNode
     axis?: string
+    onClick?: () => void
+    isActive?: boolean
 }
 
-const ListItem: FC<ListItemProps> = ({ classNames, content, startIcon, endIcon, axis = "x" }) => {
-
-
-
+const ListItem: FC<ListItemProps> = ({ classNames, content, startIcon, endIcon, axis = "x", isActive = false, onClick }) => {
 
     return (
         <div className={clsx('text-[#545778] w-full hover:cursor-pointer transition-colors relative hover:text-white justify-center flex',
-            axis == "y" && "pl-[20px] hover:pl-[18px] hover:border-l-2 hover:border-[#24FDBC] duration-150 !justify-start", 
+            axis == "y" && "pl-[20px] hover:pl-[18px] hover:border-l-2 hover:border-[#24FDBC] duration-150 !justify-start",
+            isActive == true ? axis == "y" ? "!text-white !pl-[18px] !border-l-2 !border-[#24FDBC]" : "!text-white" : "",
             classNames?.body)}
+            onClick={() => { onClick && onClick() }}
         >
             <span className="text-[16px] text-nowrap">
                 {startIcon}
